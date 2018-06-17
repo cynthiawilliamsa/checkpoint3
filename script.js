8b37333102f91918818adf12b8ed69576d1aed02
cynthiawilliamsa <cynthiawilliamsa@gmail.com>
2018-06-17 16:43:01 -0500

    image chg

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
-$(document).ready(function () {
+$(document).ready(function () {  
+  var score = $('#score');
   
-  var bones = $('#bones')
-  var score = 0;
-
-  
-
   $('#dog').on('click', function () {
     makeBone();
-    console.log(score++);
+    // console.log(score++);
+  });
+  $(".apple").on('click', function () {
+    $(this).hide();
+    clkApple();       
+  });
 
-  })
   function makeBone() {
     var numRand = Math.floor(Math.random() * 501);
     var divsize = 75;
-    var posx = (Math.random() * ($('body').width() - divsize)).toFixed();
-    var posy = (Math.random() * ($('body').height() - divsize)).toFixed();
+    var posx = (Math.random() * ($('.ground').width() - divsize)).toFixed();
+    var posy = (Math.random() * ($('.ground').height() - divsize)).toFixed();
     $newdiv = $("<div class='exploding'></div>").css({
       'left': posx + 'px',
       'top': posy + 'px'
     });
-    $newdiv.appendTo('body').delay(2000).fadeIn(300, function () {
-      //$(this).remove();
-      makeDiv();
+    $newdiv.appendTo('.ground').delay(500).fadeIn(300, function () {
+      // $newdiv.delay(2000).fadeOut(500);
+      $(this).remove();
+      
     });
   }
+  function clkApple() {
+    $('.apple').delay(7000).fadeIn(2000);
+    score+=5;
+  }
+  // function shkTree() {
+  //   $('.apple').fadeOut(100, function () {
+  //     $(this).delay(200).show();
+  //   score+=20;
+  // }
   
-  // function boneScore() {
-  //   //when bone detected on screen increment score
+});
 
-  })
+  
+
+    
+  
+
+
+
+
 
 
 
